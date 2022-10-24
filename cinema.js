a5d94c419abc91f19235347d7a9e722e7057e50f
cinehouse <guilhermehrpires@hotmail.com>
2022-10-24 19:58:55 -0300

    alteracao feita no cinema.js

---
+++ cinema.js
@@ -0,0 +1,10 @@
+const catalogo = require('./database/catalogo.json')
+
+console.log(catalogo)
+
+// listar todos os filmes que estão em cartaz (listarTodosOsFilmesEmCartaz)
+
+// buscar um filme específico pelo código dele (buscarFilme) -> parametro: codigo
+
+// alterar o status de emCartaz (true -> false e se tiver false -> true)
+// (alterarStatusEmCartaz) -> parametro: codigo
